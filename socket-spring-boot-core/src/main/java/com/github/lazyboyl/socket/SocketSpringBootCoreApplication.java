7347c60a8acad36c06a74d165bfb142bd86e2c2a
林泽锋 <282245889@qq.com>
2020-07-20 17:21:44 +0800

    Initial commit

---
+++ socket-spring-boot-core/src/main/java/com/github/lazyboyl/socket/SocketSpringBootCoreApplication.java
@@ -0,0 +1,13 @@
+package com.github.lazyboyl.socket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketSpringBootCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocketSpringBootCoreApplication.class, args);
+    }
+
+}
